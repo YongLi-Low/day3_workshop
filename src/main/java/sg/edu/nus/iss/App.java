24058cccc972066909023fd47715071f15c1b9d7
Yongli <yl.low91@gmail.com>
2023-01-16 15:34:13 +0800

    added more functions to the shopping cart

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -2,6 +2,7 @@ package sg.edu.nus.iss;
 
 import java.io.Console;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -18,12 +19,15 @@ public final class App {
      * @param args The arguments of the program.
      */
 
-    public static void main(String[] args) {
-        String dirPath = "/data2";
+    public static void main(String[] args) throws IOException {
+        String dirPath = "data2";
         String fileName = "";
 
+        // instantiate a file/directory object
         File newDirectory = new File(dirPath);
         
+        // if firectory exist, print to console 'directory exist' message
+        // else create the directory
         if (newDirectory.exists()) {
             System.out.println("Directory already exist");
         }
@@ -50,32 +54,96 @@ public final class App {
                     System.out.println("'quit' to eixt the program");
                     break;
                 case "list":
-                    if (cartItems.size() > 0) {
-                        for (String item : cartItems) {
-                            System.out.println(item);
-                        }
-                    }
-                    else {
-                        System.out.println("Your cart is empty");
-                    }
+                    listCart(cartItems);
                     break;
                 case "users":
+                    listUsers(dirPath);
                     break;
                 default:
             }
 
-            if (input.startsWith("add"))
+            String strValue = "";
+            if (input.startsWith("add")) {
                 input = input.replace(',', ' ');  // replace comma with space
                 
-                String strValue = "";
                 Scanner scanner = new Scanner(input.substring(4));  // starts from the 4th character
 
                 while (scanner.hasNext()) {
                     strValue = scanner.next();
                     cartItems.add(strValue);
+                }
+            }
+
+            if (input.startsWith("delete")) {
+                cartItems = deleteCartItem(cartItems, input);
+            }
+
+            if (input.startsWith("login")) {
+                createLoginFile(input, dirPath, fileName);
             }
         }
     }
+    public static void listCart(List<String> cartItems) {
+        if (cartItems.size() > 0) {
+            for (String item : cartItems) {
+                System.out.println(cartItems.indexOf(item) + 1 + ". " + item);
+            }
+        }
+        else {
+            System.out.println("Your cart is empty");
+        }
+    }
 
-   
+    public static List<String> deleteCartItem(List<String> cartItems, String input) {
+        String strValue = "";
+        Scanner scanner = new Scanner(input.substring(7));
+
+        while (scanner.hasNext()) {
+            strValue = scanner.next();
+            int listItemIndex = Integer.parseInt(strValue) - 1;
+
+            if (listItemIndex < cartItems.size()) {
+                cartItems.remove(listItemIndex);
+            }
+            else {
+                System.out.println("Incorrect item index");
+            }  
+        }
+        return cartItems;
+    }
+
+    public static void createLoginFile(String input, String dirPath, String fileName) throws IOException {
+        input = input.replace(',', ' ');
+
+        Scanner sc = new Scanner(input.substring(6));
+
+        while (sc.hasNext()) {
+            fileName = sc.next();
+        }
+
+        // define the filepath + filename
+        // File.separator = "\"
+        File loginFile = new File(dirPath + File.separator + fileName);
+
+        // try to create a file
+        // isCreated set to true if it is a new file to create
+        // isCreated set to false if file already exist
+        boolean isCreated = loginFile.createNewFile();
+
+        if (isCreated) {
+            System.out.println("New file created successfully" + loginFile.getCanonicalPath());
+        }
+        else {
+            System.out.println("File already created.");
+        }
+    }
+
+    public static void listUsers(String dirPath) {
+        File directoryPath = new File(dirPath);
+        String contents[] = directoryPath.list();
+        
+        for (String file : contents) {
+            System.out.println(file);
+        }
+    }
 }
